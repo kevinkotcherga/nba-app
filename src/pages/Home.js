e731381ac57f185b7d99a19b365d734df14afb9c
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-15 10:12:07 +0200

    feat(pages) : ajout de la page Home

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	background-color: #212121;
+	min-height: 100vh;
+`;
+
+const Home = () => {
+	return <Container></Container>;
+};
+
+export default Home;
